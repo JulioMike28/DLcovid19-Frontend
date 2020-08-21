438a53cc64a15b7bda604a318ec8205beab1f454
JulioMike28 <juliomike17@gmail.com>
2020-08-21 10:18:34 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DiagnoseComponent } from './diagnose/diagnose.component';
+import { ResultadoComponent } from './resultado/resultado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'diagnose', component: DiagnoseComponent},
+  {path:'resultado', component:ResultadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
